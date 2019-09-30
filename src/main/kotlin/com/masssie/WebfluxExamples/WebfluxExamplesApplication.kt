d13e607490b1785fc9c27135df206b8f44e035e4
Sean Massie <sean.massie@kroger.com>
2019-09-30 12:09:27 -0700

    base spring initializr project

---
+++ src/main/kotlin/com/masssie/WebfluxExamples/WebfluxExamplesApplication.kt
@@ -0,0 +1,11 @@
+package com.masssie.WebfluxExamples
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebfluxExamplesApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebfluxExamplesApplication>(*args)
+}
